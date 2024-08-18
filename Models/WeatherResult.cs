8c38fd524667c30b7c8b4ca43a149919b4ce609e
pollyandhiscat <pollyandhiscat@outlook.com>
2024-08-18 14:30:12 -0700

    Add new Weather Result model class file, with correct file name

---
+++ Models/WeatherResult.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Code Citations:
+ * Citation #27 
+ * Citation #28
+ * 
+*/
+
+namespace WeatherResult.Models
+{
+    public class WeatherResultModel
+    {
+        public int ZipCode { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+    }
+
+    public class locationData
+    {
+
+        public string Location { get; set; }
+        public Dictionary<string, locationDataPoints> location { get; set; }
+
+    }
+
+    public class locationDataPoints
+    {
+
+        public string name { get; set; }
+        public string region { get; set; }
+        public string country { get; set; }
+        public float lat { get; set; }
+        public float lon { get; set; }
+        public string tz_id { get; set; }
+        public string localtime { get; set; }
+
+    }
+
+}
