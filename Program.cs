3d0a17e1e672ca4b008389ff31dc46faf98d550b
pollyandhiscat <pollyandhiscat@outlook.com>
2024-08-19 20:37:48 -0700

    Add setting of environment variable to track which environment (prod vs dev) the app is running in

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using System.Net.Http;
 using Azure.Core;
 using Azure.Security.KeyVault.Secrets;
+
 var builder = WebApplication.CreateBuilder(args);
 
 /*
@@ -19,55 +20,27 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient("weatherAPIClient", client =>
 {
 
-    client.BaseAddress = new Uri("http://api.weatherapi.com/v1/");
+    client.BaseAddress = new Uri($"http://api.weatherapi.com/v1/v1/");
 
 });
 
-SecretClientOptions options = new SecretClientOptions()
-{
-    Retry =
-        {
-            Delay= TimeSpan.FromSeconds(2),
-            MaxDelay = TimeSpan.FromSeconds(16),
-            MaxRetries = 5,
-            Mode = RetryMode.Exponential
-         }
-};
-
-// Citation #36, Citation #37
-var client = new SecretClient(new Uri("https://weather-app-key-vault.vault.azure.net/"), new DefaultAzureCredential(), options);
-
-KeyVaultSecret secret = client.GetSecret("WeatherApplicationSecret");
-
-string secretValue = secret.Value;
-
 var app = builder.Build();
 
-// Citation #9, Citation #10, Citation #11
-// If the app is running in development, we use local settings such as environment variables to authenticate with Azure.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsProduction())
 {
-    // TODO: We may need to restore this logic once we deploy to Azure, but make this happen for Production, not Dev.
 
-    //var keyVaultEndpoint = new Uri(builder.Configuration["AZURE_KEY_VAULT_URI"]);
-    //var azureSecret = builder.Configuration["AZURE_CLIENT_SECRET"];
-    //var azureClientID = builder.Configuration["AZURE_CLIENT_ID"];
-    //var azureTenantID = builder.Configuration["AZURE_TENANT_ID"];
-    //builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new ClientSecretCredential(azureTenantID, azureClientID, azureSecret));
-    //var weatherAPIKey = builder.Configuration["WeatherApplicationSecret"];
-    
+    Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "PRODUCTION");
 }
 
-// If the app is running in production, we do a different authentication to Azure Key Vault.
 else
+{
 
-{ 
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-
-    // Code to authenticate with Azure a different way.
+    Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "DEVELOPMENT");
 }
+
+app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+app.UseHsts();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -77,15 +50,13 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.MapControllerRoute(
 
     name: "WeatherResult",
-    pattern: "{controller=WeatherResultController}/{action=RetrieveWeather}/");
+    pattern: "{controller=WeatherResultController}/{action=RetrieveWeather}/"
+    );
 
-// Citation #36
-app.MapGet("/", () => secretValue);
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
