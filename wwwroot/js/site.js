1740988eb64202eb277558434f970dcde27ef33c
pollyandhiscat <pollyandhiscat@outlook.com>
2024-08-29 11:57:57 -0700

    Add JavaScript function 'validateRequiredFields()' to validate whether a user needs to enter a city, state, or zip

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,53 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿/*
+ * Code Citations:
+ * Citation #54 
+ * Citation #56
+*/
+function validateRequiredFields() {
 
-// Write your JavaScript code.
+    /*
+
+     Determines if the user has entered either a zip code OR a city and state value.
+
+    */
+
+
+    let zip = document.getElementById("zip");
+    let city = document.getElementById("city");
+    let state = document.getElementById("state");
+
+    let zipVal = document.getElementById("zip").value;
+    let cityVal = document.getElementById("city").value;
+    let stateVal = document.getElementById("state").value;
+
+    if (cityVal == "" && stateVal == "" && zipVal == "") {
+
+        zip.required = true;
+        city.required = true;
+        state.required = true;
+        
+    }
+
+    else if (cityVal == "" && stateVal != "") {
+
+        zip.required = false;
+    }
+
+    else if (cityVal != "" && stateVal == "") {
+
+        zip.required = false;
+    }
+
+    if (zipVal != "") {
+
+        city.required = false;
+        state.required = false;
+
+    }
+
+    else if (cityVal != "" && stateVal != "") {
+
+        zip.required = false;
+    }
+
+}
