32645a353a2f9b136276b236f23d1d7590af683d
pdiddychickenpunk <pdiddychickenpunk@gmail.com>
2024-09-07 15:14:48 -0700

    Add model for historical location information container record

---
+++ Models/HistoricalLocationInformationModel.cs
@@ -0,0 +1,19 @@
+﻿namespace HistoricalInformation.Models
+{
+    public class HistoricalLocationInformationModel
+    {
+
+          public string id { get; set; }
+          public string category { get; set; }
+          public string country { get; set; }
+          public string state { get; set; }
+          public string localtime { get; set; }
+          public string temp_f { get; set; }
+          public string temp_c { get; set; }
+          public string wind_speed_mph { get; set; }
+          public string wind_speed_kph { get; set; }
+          public string humidity { get; set; }
+          public string uv_index { get; set; }
+
+        }
+    }
